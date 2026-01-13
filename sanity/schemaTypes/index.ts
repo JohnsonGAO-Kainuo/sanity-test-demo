f9baf5335c85e7760c3967da46ac8b89e5f7b6ab
kainuotech <kainuotech@gmail.com>
2026-01-13 12:07:18 +0800

    Add Post schema type

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import { pageType } from './page'
 import { projectType } from './project'
+import { postType } from './post'
 
-export const schemaTypes = [pageType, projectType]
+export const schemaTypes = [pageType, projectType, postType]
